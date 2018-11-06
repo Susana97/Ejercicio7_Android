18820c3bb273de07b421f3d749c6ac6ccd9fcbe5
Susana Santos Moreno <susiylola@gmail.com>
2018-11-06 19:25:12 +0100

    Ahora si!
    Arreglado bugs del toast

--- app/src/main/java/com/example/alumno/ejercicio7_android/MainActivity.java
+++ app/src/main/java/com/example/alumno/ejercicio7_android/MainActivity.java
@@ -123,6 +123,8 @@ public class MainActivity extends AppCompatActivity {
                                             mensaje.show();
                                         }
                                     });
+
+                                    break;
                                 }
                             }
                         } catch (Exception e) {
